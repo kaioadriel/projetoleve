f5f3d685c36e549803e0f5f8d223637a8d0f9af4
Kaio Adriel <kaioadrielguimaraes@gmail.com>
2026-09-06 19:32:24 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'projeto-leve-v22-metas-historicas-fotos';
-const RUNTIME_CACHE = 'projeto-leve-runtime-v22-metas-historicas-fotos';
+const CACHE_VERSION = 'projeto-leve-v23-inicializacao-corrigida';
+const RUNTIME_CACHE = 'projeto-leve-runtime-v23-inicializacao-corrigida';
 const BASE = new URL('./', self.location.href);
 const localUrl = name => new URL(name, BASE).href;
 const APP_SHELL = ['./', 'index.html', 'manifest.json', 'icons/icon-192.png', 'icons/icon-512.png'].map(localUrl);
