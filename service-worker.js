0515006badcd7be71d3750e314f6b876737acf6b
Kaio Adriel <kaioadrielguimaraes@gmail.com>
2026-09-23 20:36:55 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'projeto-leve-v98-padrao-balanca';
-const RUNTIME_CACHE = 'projeto-leve-runtime-v98-padrao-balanca';
+const CACHE_VERSION = 'projeto-leve-v99-migracao-backup';
+const RUNTIME_CACHE = 'projeto-leve-runtime-v99-migracao-backup';
 const APP_SHELL = [
   './',
   './index.html',
